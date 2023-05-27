e62108c62aeff93875619f15bd9baa58a513841f
kar im <tebakarim8@gmail.com>
2023-05-27 14:13:21 +0200

    récuperer les adresse des établissement studi avec l'api gouv

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-import AdviceComponent from "./components/AdviceComponent";
+import CompagniesComponent from "./components/CompagniesComponent";
 
 function App() {
   
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
       <Header/>
-      <AdviceComponent/>      
+      <CompagniesComponent/>      
     </>
   );
 }
