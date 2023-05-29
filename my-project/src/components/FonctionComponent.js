0b37826567b2cd8cb95151a88b255af1f0a74985
kar im <tebakarim8@gmail.com>
2023-05-29 22:21:08 +0200

    useMemo base

--- my-project/src/components/FonctionComponent.js
+++ my-project/src/components/FonctionComponent.js
@@ -1,35 +1,44 @@
-import { useState , useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 
-function FonctionComponent(props){									
-																												 
-	const [state,setState] = useState(props.value)
+function FonctionComponent(props){
+
+const [state,setState] = useState(props.value)
+
 	useEffect(()=>{
 		console.log('composant monté')
+
 		return ()=>{
 			console.log('composant va être démonté')
 }
 },[])
+
 useEffect(()=>{
 	console.log('composant mis à jour')
+
 	setState(props.value)
+
 },[props.value])
 
-return <h1>{state}</h1>
+const memoValue = useMemo(()=> state + 10, [state])
+
+return (
+<>	
+<h1>{state}</h1>
+<h2>{memoValue}</h2>
+</>
+)
+
 }
 
 export default FonctionComponent
 
-/*Ici vous utilisez deux fois le hook useEffect() 
-		de React pour gérer les effets de montée, 
-		de mise à jour et de démontage du composant. 
-		Le premier useEffect() est appelé lors de la montée du composant 
-		et ne dépend d’aucune variable, car il doit être exécuté une seule fois. 
-		Il affiche le message « composant monté » dans la console et 
-		renvoie une fonction de nettoyage qui sera exécutée lors du démontage du composant 
-		et affichera le message « composant va être démonté » 
-		dans la console.
-	Le deuxième hook useEffect() est appelé à chaque mise à jour du composant et dépend 
-	de la propriété value passée au composant en tant que 
-	props. À chaque mise à jour, il affiche le message « composant mis à jour » dans la
-	console et met également à jour l’état state en 
-	utilisant la valeur actuelle de la propriété value passée au composant en tant que props.*/
+/*Ici, le hook useMemo est utilisé pour optimiser les performances en évitant de recalculer 
+la valeur de memoValue à chaque rendu de composant. useMemo permet de stocker une valeur calculée à l’avance jusqu’à ce qu’une 
+dépendance change, auquel cas la valeur est recalculée.
+
+Dans cet exemple, memoValue est calculé en ajoutant 10 à la valeur de state. En utilisant useMemo, 
+la valeur de memoValue ne sera recalculée que si la dépendance change. Cela évite de recalculer la valeur de memoValue à chaque fois
+ que le composant est rendu.
+
+Cela peut être utile pour des calculs coûteux ou des opérations de transformation de données, où il est souhaitable de ne pas effectuer
+ le calcul à chaque rendu du composant.*/
