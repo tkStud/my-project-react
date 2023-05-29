23f77bbb934eaa6c138af4be090f7efea0e42dfb
kar im <tebakarim8@gmail.com>
2023-05-29 02:04:12 +0200

    styled-component base

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,27 +1,15 @@
 import Header from "./components/Header";
-import Modal from "./components/Modal";
-import { Button } from '@mui/material' // on importe notre composant du package mui/material de la bibliotèque mui
-import DeleteIcon from '@mui/icons-material/Delete';// import le composant DeleteIcone du package icon-material.. qui vient de la la bibliotèque mui
-
+import Toast from "./components/Toast";
 
 
 function App() {
 
-  const hClick = () =>{
-    console.log('clicked')
-  }
+ 
   
   return (
     <>
       <Header/>
-      <Modal
-      label={3}
-      type="button"
-      handleClick={hClick}
-      background='blue'
-      color='white'
-      />
-      <Button  variant="outlined" startIcon={<DeleteIcon />}>Delete</Button> {/*On peut utiliser notre boutton et notre icone */}
+      <Toast/>
     </>
   );
 }
